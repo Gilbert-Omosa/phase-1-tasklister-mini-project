4a33ed0515e2f98614c451b25d6e920b3d24d769
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-09-30 03:54:57 +0300

    add event listener for form submission, prevent default form submission behaviour

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const taskForm = document.getElementById('taskForm');
 const taskList = document.getElementById('taskList');
 
-
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// Event listener for the form submission
+taskForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
