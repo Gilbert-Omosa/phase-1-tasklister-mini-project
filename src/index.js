91198f8db09363c7f42dbb6029c0a9ecce8b669f
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-09-30 03:56:27 +0300

    get input values from the form

--- src/index.js
+++ src/index.js
@@ -5,3 +5,10 @@ const taskList = document.getElementById('taskList');
 // Event listener for the form submission
 taskForm.addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent the default form submission behavior
+
+  // Get input values from the form
+  const taskInput = document.getElementById('taskInput').value;
+  const prioritySelect = document.getElementById('prioritySelect').value;
+  const userInput = document.getElementById('userInput').value;
+  const durationInput = document.getElementById('durationInput').value;
+  const dueDateInput = document.getElementById('dueDateInput').value;
