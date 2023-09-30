0fa06eff5661fea833bd33d5f1fdb610589ca51a
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-09-30 03:50:56 +0300

    get task form and task list elements from the DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+// Get the task form and task list elements from the DOM
+const taskForm = document.getElementById('taskForm');
+const taskList = document.getElementById('taskList');
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
